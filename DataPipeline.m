69a567f73402e3e10ffeb7f9006834277b076648
cgflorides <64424507+cgflorides@users.noreply.github.com>
2021-07-13 13:01:19 +0200

    Update DataPipeline.m
    I wanted to run this script for personal purpose and i found out that the commands system and dos are doing the same thing here and mathworks suggests to use system command. Also instead of path of the script i gave the command [python main.py]

--- DataPipeline.m
+++ DataPipeline.m
@@ -1,12 +1,12 @@
 function distance = calculateDistance(time, speed)
  
     cd '<path>'
-
-    commandStr = ['C:\ProgramData\Anaconda3\python.exe main.py ',num2str(time),' ',num2str(speed)]
+    % dos and system commands are doing the same thing using different approaches. I tried this code for me and my approach needed the python and matlab scripts to be in the same directory
+    commandStr = ['python main.py ',num2str(time),' ',num2str(speed)]
     [status, commandOut] = system(commandStr);
     
-    command = 'cd';
-    [status,cmdout] = dos(command)
+    %command = 'cd';
+    %[status,cmdout] = dos(command)
     
     if status==0
     fprintf('worked');
